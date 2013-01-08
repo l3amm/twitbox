b6eb7bd865bbcbce2e31e7e19d952fd527c6a070
thaisan <thaisan@foundryhiring.com>
2013-01-07 18:57:42 -0800

    fixed shortened url regex

--- twitbox/controllers/twitter.py
+++ twitbox/controllers/twitter.py
@@ -7,7 +7,7 @@ import twitbox.lib.helpers as h
 from pylons import request, response, session, tmpl_context as c, url
 from pylons import app_globals
 from pylons.controllers.util import abort, redirect
-from twitbox.lib.parser import TwitterParser
+# from twitbox.lib.parser import TwitterParser
 
 from twitbox.lib.base import BaseController, render
 
@@ -27,24 +27,22 @@ class TwitterController(BaseController):
         else:
             abort(404)
         resp = ''
-        parser = TwitterParser()
+        urls = []
+        # parser = TwitterParser()
+        
         for tweet in tweets:
-<<<<<<< HEAD
-            m = re.search('http.*(\s+|$)', tweet.text)
+            m = re.search("http.([A-Za-z0-9\/\.]+)", tweet.text)
             if m:
                 r = requests.get(m.group(0))
                 print r.text
                 
-=======
-            # m = re.search('http.*(\s+|$)', tweet.text)
-            # if m:
-            #     response = urllib2.urlopen(m.group(0))
-            #     html = response.read()
->>>>>>> a782ec2e414d3c1ae61ad560baf3861eacb89a01
+                # long_url = h.get_redirected_url(m.group(0))
+                # urls.append(long_url)
                 
             resp += tweet.text
             resp += "\n"
         # resp = urllib2.urlopen('http://instagr.am/p/UIcpAETN73/')
         # html = resp.read()
         
-        return render("/view.html", extra_vars={'tweets': tweets})
+        
+        return render("/view.html", extra_vars={'tweets': tweets, 'urls': urls})
